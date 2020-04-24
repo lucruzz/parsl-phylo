12caa93d9a1ebebcdc6e789e44a6eba50dbebdc6
lucruzz <62730384+lucruzz@users.noreply.github.com>
2020-04-24 20:08:41 -0300

    Create phylo.py

---
+++ phylo.py
@@ -0,0 +1,94 @@
+import parsl
+import os
+from parsl.app.app import python_app, bash_app
+from parsl.configs.local_threads import config
+from pathlib import Path
+
+parsl.config.retries = 2
+parsl.load(config)
+
+p = Path('.')
+fasta = list(p.glob('./ORTHO*'))#p.glob('./input/ORTHO*')    
+fastaNumbered, ma, phylip, mg, mfMG = [], [], [], [], []
+r1, r2, r3, r4, r5, r6, r7, r8, r9, r10 = [], [], [], [], [], [], [], [], [], []
+k = 0
+
+@bash_app
+def fastanumbered(in_file, stderr = parsl.AUTO_LOGNAME, stdout = './intermediate_files/{}.fastaNumbered'.format(parsl.AUTO_LOGNAME)):
+    return 'fastaNumbered {} {}'.format(in_file, stdout)
+
+@bash_app
+def mafft(in_file, stderr = parsl.AUTO_LOGNAME, stdout = './intermediate_files/{}.mafft'.format(parsl.AUTO_LOGNAME)):
+    return 'mafft {} > {}'.format(in_file, stdout)
+
+@bash_app
+def readseq(in_file, stderr = parsl.AUTO_LOGNAME, stdout = './results/{}.phylip'.format(parsl.AUTO_LOGNAME)):
+    return 'readseq {} {}'.format(in_file, stdout)
+
+@bash_app
+def modelgenerator(in_file, stderr = parsl.AUTO_LOGNAME, stdout = './results/{}.mg'.format(parsl.AUTO_LOGNAME)):
+    return 'modelgenerator {} {}'.format(in_file, stdout)
+
+@bash_app
+def cleanModelgenerator(in_file, stderr = parsl.AUTO_LOGNAME):
+    return 'cleanModelgenerator {}'.format(in_file)
+
+@bash_app
+def raxml(phylip_file, mfMG_file, stderr = parsl.AUTO_LOGNAME):
+    return 'raxml {} {} 2 4'.format(phylip_file, mfMG_file)
+
+r_future = []
+for i in fasta:
+    fastaNumbered_future =  fastanumbered(i)# i equivale a fasta[0]
+    fastaNumbered_future.result()
+    src = Path('./intermediate_files/-1.fastaNumbered')
+    dst = Path('./intermediate_files/{}.fastaNumbered'.format(k+1))
+    os.rename(src, dst)
+    fastaNumbered.append(dst)
+    print('FASTANUMBERED: ', dst)    
+
+    mafft_future = mafft(fastaNumbered[k])
+    mafft_future.result()
+    src = Path('./intermediate_files/-1.mafft')
+    dst = Path('./intermediate_files/{}.mafft'.format(k+1))
+    os.rename(src, dst)
+    ma.append(dst)
+    print('MAFFT: ', dst)
+
+    phylip_future = readseq(ma[k])
+    phylip_future.result()
+    src = Path('./results/-1.phylip')
+    dst = Path('./results/{}.phylip'.format(k+1))
+    os.rename(src, dst)
+    phylip.append(dst)
+    print('PHYLIP: ', dst)
+
+    mg_future = modelgenerator(phylip[k])
+    mg_future.result()
+    src = Path('./results/-1.mg')
+    dst = Path('./results/{}.mg'.format(k+1))
+    os.rename(src, dst)
+    mg.append(dst)
+    print('MG: ', dst)
+
+    mfMG_future = cleanModelgenerator(mg[k])
+    mfMG_future.result()
+    dst = Path('./results/{}.mg.modelFromMG.txt'.format(k+1))
+    mfMG.append(dst)
+    print('mfMG: ', dst)
+    
+    raxml(phylip[k], mfMG[k])
+    #r_future.append(raxml(phylip[k], mfMG[k]))
+    #r_future.result()
+    k = k + 1
+
+#j = 0
+#for i in r_future:
+#    print('***%d', j)
+#    i.result()
+#    j = j + 1
+print('--> fastaNumbered: ', fastaNumbered)
+print('--> mafft: ', ma)
+print('--> phylip: ', phylip)
+print('--> mg: ', mg)
+print('--> mfMG: ', mfMG)
